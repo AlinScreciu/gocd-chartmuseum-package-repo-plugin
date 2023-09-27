3ab84316f9e80072c2beb40ad5513189d1d68fff
Alin Screciu <alin.screciu@8x8.com>
2023-09-27 11:33:08 +0300

    Fix null pointer error

--- src/main/java/com/tw/go/plugin/material/artifactrepository/chartmuseum/PackageConfig.java
+++ src/main/java/com/tw/go/plugin/material/artifactrepository/chartmuseum/PackageConfig.java
@@ -5,29 +5,33 @@ import com.tw.go.plugin.material.artifactrepository.chartmuseum.message.PackageM
 
 public class PackageConfig {
     private final String chartName;
-    private final String pollFrom;
-    private final String pollTo;
-    
+    private String pollFrom = null;
+    private String pollTo = null;
+
     public PackageConfig(PackageMaterialProperties packageConfig) {
         chartName = packageConfig.getProperty(Constants.PACKAGE_NAME).value();
-        pollFrom = packageConfig.getProperty(Constants.POLL_VERSION_FROM).value();
-        pollTo = packageConfig.getProperty(Constants.POLL_VERSION_TO).value();
+        if (packageConfig.getProperty(Constants.POLL_VERSION_FROM) != null) {
+            pollFrom = packageConfig.getProperty(Constants.POLL_VERSION_FROM).value();
+        }
+        if (packageConfig.getProperty(Constants.POLL_VERSION_TO) != null) {
+            pollTo = packageConfig.getProperty(Constants.POLL_VERSION_TO).value();
+        }
     }
-    
+
     public PackageConfig(String chartName, String pollFrom, String pollTo) {
         this.chartName = chartName;
         this.pollFrom = pollFrom;
         this.pollTo = pollTo;
     }
-    
+
     public String getChartName() {
         return chartName;
     }
-    
+
     public String getPollFrom() {
         return pollFrom;
     }
-    
+
     public String getPollTo() {
         return pollTo;
     }
